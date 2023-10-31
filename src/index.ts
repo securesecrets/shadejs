1708c34c35e10ea8def096d18c87f922717e005f
Austin <awwoetzel@gmail.com>
2023-10-31 14:30:54 -0500

    feat: export functions from main

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import '~/polyfills/index';
 import { getSecretNetworkClient$, getActiveQueryClient$ } from '~/client';
 import { queryPrice$, queryPrices$ } from '~/contracts/services/oracle';
+import { batchQuery$ } from '~/contracts/services/batchQuery';
+import {
+  queryFactoryConfig$,
+  queryFactoryPairs$,
+  queryPairConfig$,
+  batchQueryPairsInfo$,
+  batchQueryStakingInfo$,
+} from '~/contracts/services/swap';
 
 export {
   getSecretNetworkClient$,
   getActiveQueryClient$,
   queryPrice$,
   queryPrices$,
+  batchQuery$,
+  queryFactoryConfig$,
+  queryFactoryPairs$,
+  queryPairConfig$,
+  batchQueryPairsInfo$,
+  batchQueryStakingInfo$,
 };
