a30049293983997241e46e704410a6f5c2d2e6d3
Austin <awwoetzel@gmail.com>
2023-11-10 13:24:21 -0600

    feat: batch swap queryies tests and docs

---
+++ src/test/mocks/swap/batchStakingConfigParsed.ts
@@ -0,0 +1,136 @@
+const batchStakingConfigParsed = [
+  {
+    stakingContractAddress: 'secret17ue98qd2akjazu2w2r95cz06mh8pfl3v5hva4j',
+    stakingInfo: {
+      lpTokenContract: {
+        address: 'secret1ffre8nf653pem9hn5f4ep5pg70dd837tucgdyv',
+        codeHash: 'b0c2048d28a0ca0b92274549b336703622ecb24a8c21f417e70c03aa620fcd7b',
+      },
+      pairContractAddress: 'secret1wn9tdlvut2nz0cpv28qtv74pqx20p847j8gx3w',
+      adminAuthContract: {
+        address: 'secret1hcz23784w6znz3cmqml7ha8g4x6s7qq9v93mtl',
+        codeHash: '6666d046c049b04197326e6386b3e65dbe5dd9ae24266c62b333876ce57adaa8',
+      },
+      queryAuthContract: {
+        address: 'secret1nd56rl8n63auxmwekkyqqq030paeqpap3dmw0w',
+        codeHash: '1b7a863cd327d24323bfd067dbb975e03dc8a51edee55717ad6b9849b3d35234',
+      },
+      totalStakedAmount: '3754942571',
+      rewardTokens: [
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '83912',
+          rewardPerStakedToken: '3',
+          validTo: 1688490000,
+          lastUpdated: 1688490000,
+        },
+      ],
+    },
+  },
+  {
+    stakingContractAddress: 'secret1vgtmfvzdn7ztn7kcrqd7p6f2z97wvauavp3udh',
+    stakingInfo: {
+      lpTokenContract: {
+        address: 'secret10egcg03euavu336fzed87m4zdx8jkgzzz7zgmh',
+        codeHash: 'b0c2048d28a0ca0b92274549b336703622ecb24a8c21f417e70c03aa620fcd7b',
+      },
+      pairContractAddress: 'secret1qyt4l47yq3x43ezle4nwlh5q0sn6f9sesat7ap',
+      adminAuthContract: {
+        address: 'secret1hcz23784w6znz3cmqml7ha8g4x6s7qq9v93mtl',
+        codeHash: '6666d046c049b04197326e6386b3e65dbe5dd9ae24266c62b333876ce57adaa8',
+      },
+      queryAuthContract: {
+        address: 'secret1nd56rl8n63auxmwekkyqqq030paeqpap3dmw0w',
+        codeHash: '1b7a863cd327d24323bfd067dbb975e03dc8a51edee55717ad6b9849b3d35234',
+      },
+      totalStakedAmount: '84213465469',
+      rewardTokens: [
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '33564',
+          rewardPerStakedToken: '0',
+          validTo: 1683046800,
+          lastUpdated: 1683046800,
+        },
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '28935',
+          rewardPerStakedToken: '0',
+          validTo: 1688490000,
+          lastUpdated: 1688490000,
+        },
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '28935',
+          rewardPerStakedToken: '0',
+          validTo: 1690305188,
+          lastUpdated: 1690305188,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '0',
+          rewardPerStakedToken: '0',
+          validTo: 1682341200,
+          lastUpdated: 1682341200,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '1618',
+          rewardPerStakedToken: '0',
+          validTo: 1684944000,
+          lastUpdated: 1684944000,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '788',
+          rewardPerStakedToken: '0',
+          validTo: 1687534200,
+          lastUpdated: 1687534200,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '13606',
+          rewardPerStakedToken: '0',
+          validTo: 1687633200,
+          lastUpdated: 1687633200,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            codeHash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          rewardPerSecond: '453',
+          rewardPerStakedToken: '0',
+          validTo: 1690131600,
+          lastUpdated: 1690131600,
+        },
+      ],
+    },
+  },
+];
+
+export { batchStakingConfigParsed };
---
+++ src/test/mocks/swap/batchStakingConfigUnparsed.ts
@@ -0,0 +1,147 @@
+const batchStakingConfigUnparsed = [
+  {
+    id: 'secret17ue98qd2akjazu2w2r95cz06mh8pfl3v5hva4j',
+    response: {
+      lp_token: {
+        address: 'secret1ffre8nf653pem9hn5f4ep5pg70dd837tucgdyv',
+        code_hash: 'b0c2048d28a0ca0b92274549b336703622ecb24a8c21f417e70c03aa620fcd7b',
+      },
+      amm_pair: 'secret1wn9tdlvut2nz0cpv28qtv74pqx20p847j8gx3w',
+      admin_auth: {
+        address: 'secret1hcz23784w6znz3cmqml7ha8g4x6s7qq9v93mtl',
+        code_hash: '6666d046c049b04197326e6386b3e65dbe5dd9ae24266c62b333876ce57adaa8',
+      },
+      query_auth: {
+        address: 'secret1nd56rl8n63auxmwekkyqqq030paeqpap3dmw0w',
+        code_hash: '1b7a863cd327d24323bfd067dbb975e03dc8a51edee55717ad6b9849b3d35234',
+      },
+      total_amount_staked: '3754942571',
+      reward_tokens: [
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 8,
+          reward_per_second: '83912',
+          reward_per_staked_token: '3',
+          valid_to: 1688490000,
+          last_updated: 1688490000,
+        },
+      ],
+    },
+  },
+  {
+    id: 'secret1vgtmfvzdn7ztn7kcrqd7p6f2z97wvauavp3udh',
+    response: {
+      lp_token: {
+        address: 'secret10egcg03euavu336fzed87m4zdx8jkgzzz7zgmh',
+        code_hash: 'b0c2048d28a0ca0b92274549b336703622ecb24a8c21f417e70c03aa620fcd7b',
+      },
+      amm_pair: 'secret1qyt4l47yq3x43ezle4nwlh5q0sn6f9sesat7ap',
+      admin_auth: {
+        address: 'secret1hcz23784w6znz3cmqml7ha8g4x6s7qq9v93mtl',
+        code_hash: '6666d046c049b04197326e6386b3e65dbe5dd9ae24266c62b333876ce57adaa8',
+      },
+      query_auth: {
+        address: 'secret1nd56rl8n63auxmwekkyqqq030paeqpap3dmw0w',
+        code_hash: '1b7a863cd327d24323bfd067dbb975e03dc8a51edee55717ad6b9849b3d35234',
+      },
+      total_amount_staked: '84213465469',
+      reward_tokens: [
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 8,
+          reward_per_second: '33564',
+          reward_per_staked_token: '0',
+          valid_to: 1683046800,
+          last_updated: 1683046800,
+        },
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 8,
+          reward_per_second: '28935',
+          reward_per_staked_token: '0',
+          valid_to: 1688490000,
+          last_updated: 1688490000,
+        },
+        {
+          token: {
+            address: 'secret153wu605vvp934xhd4k9dtd640zsep5jkesstdm',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 8,
+          reward_per_second: '28935',
+          reward_per_staked_token: '0',
+          valid_to: 1690305188,
+          last_updated: 1690305188,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 6,
+          reward_per_second: '0',
+          reward_per_staked_token: '0',
+          valid_to: 1682341200,
+          last_updated: 1682341200,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 6,
+          reward_per_second: '1618',
+          reward_per_staked_token: '0',
+          valid_to: 1684944000,
+          last_updated: 1684944000,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 6,
+          reward_per_second: '788',
+          reward_per_staked_token: '0',
+          valid_to: 1687534200,
+          last_updated: 1687534200,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 6,
+          reward_per_second: '13606',
+          reward_per_staked_token: '0',
+          valid_to: 1687633200,
+          last_updated: 1687633200,
+        },
+        {
+          token: {
+            address: 'secret1dtdgpn0v8vzt2zz2an0anawxj26llne3ugl9ek',
+            code_hash: '638a3e1d50175fbcb8373cf801565283e3eb23d88a9b7b7f99fcc5eb1e6b561e',
+          },
+          decimals: 6,
+          reward_per_second: '453',
+          reward_per_staked_token: '0',
+          valid_to: 1690131600,
+          last_updated: 1690131600,
+        },
+      ],
+    },
+  },
+];
+
+export {
+  batchStakingConfigUnparsed,
+};
