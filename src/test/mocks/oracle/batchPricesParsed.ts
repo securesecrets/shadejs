4f74875b1ff367c3f36924b3e48508f030589414
Austin <awwoetzel@gmail.com>
2024-02-13 16:39:37 -0600

    feat: oracle batch query for individual prices

---
+++ src/test/mocks/oracle/batchPricesParsed.ts
@@ -0,0 +1,22 @@
+import { OracleErrorType, ParsedOraclePricesResponse } from '~/types';
+
+const batchPricesWithErrorParsedResponse: ParsedOraclePricesResponse = {
+  BTC: {
+    oracleKey: 'BTC',
+    rate: '49521843708800000000000',
+    lastUpdatedBase: 1707858482,
+    // eslint-disable-next-line
+    lastUpdatedQuote: 18446744073709551615,
+  },
+  'Quicksilver ATOM': {
+    oracleKey: 'Quicksilver ATOM',
+    error: {
+      type: OracleErrorType.STALE_DERIVATIVE_RATE,
+      msg: 'Cannot parse response: expected value at line 1 column 1 in: Generic error: Querier system error: Cannot parse response: expected value at line 1 column 1 in: Generic error: Derivative rate is stale. Last updated 1704806636. Current time 1707858606.',
+    },
+  },
+};
+
+export {
+  batchPricesWithErrorParsedResponse,
+};
