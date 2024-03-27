5cc7110c1e1ba3196c1b5229f37ea39910b853b2
Austin <awwoetzel@gmail.com>
2024-03-27 09:48:09 -0500

    feat: new public endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // default endpoint to be used when alternative endpoint is not provided
 // this is a public endpoint and no guarantees can be provided about the performance
-const DEFAULT_SECRET_LCD_ENDPOINT = 'https://lcd.secret.express/';
+const DEFAULT_SECRET_LCD_ENDPOINT = 'https://lcd.mainnet.secretsaturn.net/';
 const SECRET_MAINNET_CHAIN_ID = 'secret-4';
 
 export {
