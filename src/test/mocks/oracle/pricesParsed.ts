808a867934119bb995ad135d023565ffb35a909d
Austin <awwoetzel@gmail.com>
2024-04-19 09:00:33 -0500

    feat: oracle queries converte to query router

--- src/test/mocks/oracle/pricesParsed.ts
+++ src/test/mocks/oracle/pricesParsed.ts
@@ -3,7 +3,7 @@ const priceParsed = {
   rate: '27917207155600000000000',
   lastUpdatedBase: 1696644063,
   lastUpdatedQuote: 18446744073709552000,
-  blockHeight: undefined,
+  blockHeight: 123456789,
 };
 
 const pricesParsed = {
@@ -12,12 +12,14 @@ const pricesParsed = {
     rate: '27917207155600000000000',
     lastUpdatedBase: 1696644063,
     lastUpdatedQuote: 18446744073709552000,
+    blockHeight: 3,
   },
   ETH: {
     oracleKey: 'ETH',
     rate: '1644083682900000000000',
     lastUpdatedBase: 1696644063,
     lastUpdatedQuote: 18446744073709552000,
+    blockHeight: 3,
   },
 };
 
